f37b5bbedda74ee6151b89b36891ec25aecb2c83
Kenzo <hohochan66@gmail.com>
2022-05-04 23:23:24 +0800

    basic structure

---
+++ src/pages/Home/index.js
@@ -0,0 +1,11 @@
+import Edit from './components/Edit'
+import List from './components/List'
+//import Item from './components/Item'
+
+const Home = () => {
+    return <div>
+        <Edit />
+        <List />
+    </div>
+}
+export default Home
